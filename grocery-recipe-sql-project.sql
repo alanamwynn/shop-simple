c270bb67d538fe7d9f28f73c8240ee45ba9129c9
Alana Wynn <alanamwynn@gmail.com>
2023-09-23 12:45:34 -0600

    revised tables and relationships

--- grocery-recipe-sql-project.sql
+++ grocery-recipe-sql-project.sql
@@ -1,21 +1,17 @@
 CREATE TABLE "product" (
   "id" integer PRIMARY KEY,
   "name" text,
-  "brand" text
-);
-
-CREATE TABLE "unit_price" (
-  "id" integer,
-  "price" float,
-  "product_id" integer
+  "brand" text,
+  "food_type_id" integer
 );
 
 CREATE TABLE "ingredient" (
-  "id" integer,
+  "id" integer PRIMARY KEY,
   "name" text,
   "amount" float,
   "unit" text,
-  "recipe_id" integer
+  "recipe_id" integer,
+  "food_type_id" integer
 );
 
 CREATE TABLE "recipe" (
@@ -31,17 +27,18 @@ CREATE TABLE "package_type" (
   "unit_of_measurement" text,
   "quantity" integer,
   "product_id" integer,
-  "price" float,
-  "unit_price_id" integer
+  "price" float
 );
 
-
-COMMENT ON COLUMN "posts"."body" IS 'Content of the post';
+CREATE TABLE "food_type" (
+  "id" integer PRIMARY KEY,
+  "name" text
+);
 
 ALTER TABLE "package_type" ADD FOREIGN KEY ("product_id") REFERENCES "product" ("id");
 
 ALTER TABLE "ingredient" ADD FOREIGN KEY ("recipe_id") REFERENCES "recipe" ("id");
 
-ALTER TABLE "package_type" ADD FOREIGN KEY ("id") REFERENCES "unit_price" ("product_id");
+ALTER TABLE "product" ADD FOREIGN KEY ("food_type_id") REFERENCES "food_type" ("id");
 
-
+ALTER TABLE "ingredient" ADD FOREIGN KEY ("food_type_id") REFERENCES "food_type" ("id");
