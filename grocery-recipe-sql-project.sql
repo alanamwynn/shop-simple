ef8c3f7bbf734cd480d62046431e4ad1f147b519
Alana Wynn <alanamwynn@gmail.com>
2023-09-23 12:07:52 -0600

    removed erroneous tables

--- grocery-recipe-sql-project.sql
+++ grocery-recipe-sql-project.sql
@@ -35,27 +35,6 @@ CREATE TABLE "package_type" (
   "unit_price_id" integer
 );
 
-CREATE TABLE "follows" (
-  "following_user_id" integer,
-  "followed_user_id" integer,
-  "created_at" timestamp
-);
-
-CREATE TABLE "users" (
-  "id" integer PRIMARY KEY,
-  "username" varchar,
-  "role" varchar,
-  "created_at" timestamp
-);
-
-CREATE TABLE "posts" (
-  "id" integer PRIMARY KEY,
-  "title" varchar,
-  "body" text,
-  "user_id" integer,
-  "status" varchar,
-  "created_at" timestamp
-);
 
 COMMENT ON COLUMN "posts"."body" IS 'Content of the post';
 
@@ -65,8 +44,4 @@ ALTER TABLE "ingredient" ADD FOREIGN KEY ("recipe_id") REFERENCES "recipe" ("id"
 
 ALTER TABLE "package_type" ADD FOREIGN KEY ("id") REFERENCES "unit_price" ("product_id");
 
-ALTER TABLE "posts" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("id");
 
-ALTER TABLE "follows" ADD FOREIGN KEY ("following_user_id") REFERENCES "users" ("id");
-
-ALTER TABLE "follows" ADD FOREIGN KEY ("followed_user_id") REFERENCES "users" ("id");
